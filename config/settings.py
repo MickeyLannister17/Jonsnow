66cd6f4277d9de936d997f2cc854c128a38cfb00
Senaike Michael <70372483+MickeyLannister17@users.noreply.github.com>
2021-04-27 21:05:05 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 STATIC_ROOT = STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStaticStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStaticStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
