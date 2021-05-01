1dccf8cacfcb1e42bb419c5be3322c0879700d48
Senaike Michael <senaikepooldis@gmail.com>
2021-05-01 01:04:10 +0100

    Fix: config for deployment

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import django_heroku
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,12 +19,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j=hdo_6qw90pecuwj7nuw$^(v)q!z&h-e4c#ejlz&z-bwl%%r2'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['DEBUG']
 
-ALLOWED_HOSTS = ['bravosapp.herokuapp.com']
+ALLOWED_HOSTS = ['senaikepooldis.herokuapp.com']
 
 # Application definition
 
@@ -129,4 +129,3 @@ LOGOUT_REDIRECT_URL = 'home'
 import dj_database_url
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
-django_heroku.settings(locals())
